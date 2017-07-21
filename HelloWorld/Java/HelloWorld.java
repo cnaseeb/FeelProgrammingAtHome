a276891f439ea235d373aef48b1903f0e2fc7a72
Chan <cnaseeb@users.noreply.github.com>
2017-07-21 17:24:53 +0200

    Create HelloWorld.java
    Hello World in Java

---
+++ HelloWorld/Java/HelloWorld.java
@@ -0,0 +1,26 @@
+##############################################################################
+#
+# Copyright 2017 Chan Naseeb (unless otherwise stated)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
+public class HelloWorld{
+  public static void main(String[] args){
+    // Prints "Hello, World" to the console
+    System.out.println("Hello, World");
+  }
+}
+
+
+Copyright © 2017, Chan Naseeb.
